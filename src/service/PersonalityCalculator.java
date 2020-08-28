b7388079b7499d1bc2a5909a7881bb673779af37
FACEPrep-ProGrad <prograd@faceprep.in>
2020-08-28 20:04:50 +0530

    code updated

--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -32,20 +32,21 @@ public class PersonalityCalculator
 
 	public String findYourBrainType(String options)
 	{
-		int value[]=findAnswers(options);
-		int n=value.length;
-		int a=value[0]+value[1]+value[2]+value[4]+value[7]+value[9]+value[10]+value[11]+value[13]+value[17]+value[19];
-		int b=value[3]+value[5]+value[6]+value[8]+value[12]+value[14]+value[15]+value[16]+value[18];
-		int sum=66-a+b;
-		System.out.println("Your total score is "+sum+".");
-		if((sum>=22)&&(sum<=55))
-			return "leftbrained";
-		if((sum>=56)&&(sum<=64))
-			return "No clear preference";
-		if((sum>=65)&&(sum<=100))
-             return "rightbrained";
-		else
-		     return "false";	
+//		int value[]=findAnswers(options);
+//		int n=value.length;
+//		int a=value[0]+value[1]+value[2]+value[4]+value[7]+value[9]+value[10]+value[11]+value[13]+value[17]+value[19];
+//		int b=value[3]+value[5]+value[6]+value[8]+value[12]+value[14]+value[15]+value[16]+value[18];
+//		int sum=66-a+b;
+//		System.out.println("Your total score is "+sum+".");
+//		if((sum>=22)&&(sum<=55))
+//			return "leftbrained";
+//		if((sum>=56)&&(sum<=64))
+//			return "No clear preference";
+//		if((sum>=65)&&(sum<=100))
+//             return "rightbrained";
+//		else
+//		     return "false";	
+		return null;
 		
 	}	
 }
